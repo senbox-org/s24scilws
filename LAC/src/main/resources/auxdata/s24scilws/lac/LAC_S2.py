1f13b2e8842657903a47cc3729bac4dcfd719ac0
Martino Ferrari <manda.mgf@gmail.com>
2020-10-13 15:38:00 +0200

    Fixed argument issue, improved error output

--- LAC/src/main/resources/auxdata/s24scilws/lac/LAC_S2.py
+++ LAC/src/main/resources/auxdata/s24scilws/lac/LAC_S2.py
@@ -3,25 +3,50 @@
 import os, sys, argparse, subprocess
 from datetime import datetime,timedelta
 
-parser = argparse.ArgumentParser(description='Download and process S2 data')
-parser.add_argument('-dirin','--dirin', required=True, help='(e.g: /home)')
-parser.add_argument('-atmcor','--atmcor', required=True, help='(e.g: y means full correction while n means only Rayleigh)')
-parser.add_argument('-bands','--bands', required=False, help='(e.g: Set the bands you want as output : 02_03_04_11 as default)')
-parser.add_argument('-dirout','--dirout', required=True, help='(e.g: /home)')
 
-print("***HERE***")
-print(sys.args)
+def parse_args(argv, struct):
+    args = {}
+    argv = [arg.replace('-', '').replace(' ', '').strip() if arg.startswith('-') else arg for arg in argv]
 
-args = parser.parse_args()
+    for key in struct:
+        if key in argv:
+            i = argv.index(key)
+            if i+1 < len(argv) and argv[i+1] not in struct.keys():
+                args[key] = argv[i+1]
+            else:
+                print(f'Misformed argument: -{key} or --{key}')
+                sys.exit(1)
+        else:
+            if struct[key][1] is None:
+                print(f'Missing argument: -{key} or --{key}')
+                sys.exit(1)
+            else:
+                args[key] = struct[key][1]
+    return args
 
-DirIn =str(args.dirin)
-Bands = str(args.bands)
-Atmcor=str(args.atmcor)
-DirOut=str(args.dirout)
+
+argv = sys.argv[1:]
+
+arg_struct = {
+    'dirin': ('(e.g: /home)', None),
+    'dirout': ('e.g: /home)', None),
+    'atmcor': ('(e.g: y means full correction while n means only Rayleigh)', None),
+    'bands': ('(e.g: Set the bands you want as output : 02_03_04_11 as default)', '02_03_04_11'),
+}
+
+
+
+args = parse_args(argv, arg_struct)
+
+DirIn =str(args['dirin'])
+Bands = str(args['bands'])
+Atmcor=str(args['atmcor'])
+DirOut=str(args['dirout'])
 
 
 # Level=str(args.level)
 
+print('Ready to process')
 Level="L1C"
 PixSat="0.15"
 #DirScriptDirScript="/mount/internal/work-st/projects/esrin-079/1402-seom/S2/scripts/S2_L8/S2"
@@ -31,6 +56,7 @@ PixSat="0.15"
 #print ("python "+" SEOM_Rayleigh_LAC.py "+DirOut+" \""+Bands+"\" "+PixSat)
 if os.path.exists(os.path.join(DirIn, "MTD_TL.xml")): #TEST pour voir s'il y a un fichier telecharge a la date donnee
     if Atmcor=="n":	
+        print('Process without full correction')
         #test=subprocess.check_output("python "+DirScript+"/SEOM_Rayleigh_SCIHUB.py "+DirOut+"/"+Code+"/"+TDateTemp+" \""+Bands+"\" "+PixSat, shell=True)
         test=subprocess.Popen(["python", "SEOM_Rayleigh_LAC.py", DirIn, Bands, PixSat, DirOut], stderr=subprocess.PIPE)
         output,error=test.communicate()
@@ -53,3 +79,6 @@ if os.path.exists(os.path.join(DirIn, "MTD_TL.xml")): #TEST pour voir s'il y a u
                 sys.exit(1)
         if output is not None:
             print (output)
+else:
+    print(f'Error: input directory `{DirI}` does not contain the `MTD_TL.xml` file')
+    sys.exit(1)
