f230bd26c3e6a70209ea80f78f1fd7f3c54cec7c
Martino Ferrari <manda.mgf@gmail.com>
2020-12-21 09:57:21 +0100

    using xml metadata file for product name.

--- s24scilws-lac-reader/src/main/java/org/esa/s2tbx/s24scilws/lac/dataio/LacProductReader.java
+++ s24scilws-lac-reader/src/main/java/org/esa/s2tbx/s24scilws/lac/dataio/LacProductReader.java
@@ -3,6 +3,7 @@ package org.esa.s2tbx.s24scilws.lac.dataio;
 import com.bc.ceres.core.ProgressMonitor;
 import org.esa.s2tbx.dataio.VirtualDirEx;
 import org.esa.s2tbx.dataio.readers.BaseProductReaderPlugIn;
+import org.esa.s2tbx.dataio.s2.S2BandConstants;
 import org.esa.snap.core.dataio.AbstractProductReader;
 import org.esa.snap.core.dataio.ProductReaderPlugIn;
 import org.esa.snap.core.datamodel.Band;
@@ -20,6 +21,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Arrays;  
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Node;
 
 /**
  * Created by obarrile on 25/12/2019.
@@ -30,17 +39,25 @@ public class LacProductReader extends AbstractProductReader {
     private VirtualDirEx virtualDir;
     private List<Product> associatedProducts;
 
-
     protected LacProductReader(ProductReaderPlugIn readerPlugIn) {
         super(readerPlugIn);
         this.associatedProducts = new ArrayList<>();
+    }
+
+    private static S2BandConstants getBandFromName(String name) {
+        for (S2BandConstants band : S2BandConstants.values()) {
+            if (band.getFilenameBandId().equals(name))
+                return band;
+        }
+        return null;
     }
 
     @Override
     protected Product readProductNodesImpl() throws IOException {
 
         Path inputPath = BaseProductReaderPlugIn.convertInputToPath(super.getInput());
-        if(inputPath.getFileName().equals("MTD_LAC.xml")) {
+        Path metadaPath = inputPath.normalize();
+        if (inputPath.getFileName().toString().equals("MTD_LAC.xml")) {
             inputPath.resolveSibling("GRANULE");
         }
 
@@ -53,46 +70,82 @@ public class LacProductReader extends AbstractProductReader {
             throw new NullPointerException("The virtual dir is null for input path '" + inputPath.toString() + "'.");
         }
 
-        //read all Tifs
+        // read all Tifs
         ArrayList<Band> bandList = new ArrayList<>();
-        Band referenceBand = null; //THis will be the largest band
+        Band referenceBand = null; // THis will be the largest band
         String[] files = this.virtualDir.listAll();
+        Arrays.sort(files);
         for (String file : files) {
             if (file.endsWith(".TIF")) {
-                Band band = readGeoTiffProductBand(file,0); //all TIFs in this product should have only one band
+                Band band = readGeoTiffProductBand(file, 0); // all TIFs in this product should have only one band
                 bandList.add(band);
-                if(referenceBand == null || referenceBand.getRasterWidth() < band.getRasterWidth()) {
+                if (referenceBand == null || referenceBand.getRasterWidth() < band.getRasterWidth()) {
                     referenceBand = band;
                 }
             }
         }
+        File file = metadaPath.toFile();
+        // an instance of factory that gives a document builder
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        // an instance of builder to parse the specified xml file
+        DocumentBuilder db;
+        try {
+            db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);  
+            doc.getDocumentElement().normalize();  
+            Node node = doc.getElementsByTagName("PRODUCT_URI").item(0);  
+            //create Product
+            Product product = new Product(node.getTextContent(), "LAC", referenceBand.getRasterWidth(), referenceBand.getRasterHeight());
+            product.setDescription("LAC output");
+            // set File Location
+            product.setFileLocation(inputPath.toFile());
 
-        //create Product
-        Product product = new Product("output_DATA", "LAC", referenceBand.getRasterWidth(), referenceBand.getRasterHeight());
-        product.setDescription("LAC output");
-        // set File Location
-        product.setFileLocation(inputPath.toFile());
+            product.setSceneGeoCoding(referenceBand.getGeoCoding());
+            product.setAutoGrouping("Bands");
 
-        product.setSceneGeoCoding(referenceBand.getGeoCoding());
-        product.setAutoGrouping("Bands");
+            //addBands
+            for(Band srcBand : bandList) {
+                String file_name = srcBand.getProduct().getName();
+                String band_name = file_name;
+                boolean visual_band = false;
+                S2BandConstants bandInfo = null;
+                if (file_name.startsWith("O3-")){
+                    band_name = "LAC_O3";
+                } else if (file_name.startsWith("C_")){
+                    band_name = file_name.split("_")[3];
+                    bandInfo = LacProductReader.getBandFromName(band_name);
+                    band_name = bandInfo.getPhysicalName();
+                    visual_band = true;
 
-        //addBands
-        for(Band srcBand : bandList) {
-            Band targetBand = new Band(srcBand.getProduct().getName(), srcBand.getDataType(), srcBand.getRasterWidth(), srcBand.getRasterHeight());
-            product.addBand(targetBand);
-            ProductUtils.copyGeoCoding(srcBand, targetBand);
-            targetBand.setNoDataValue(srcBand.getNoDataValue());
-            targetBand.setNoDataValueUsed(true);
-            //targetBand.setSpectralWavelength(metadata.getCentralWavelength(bandId)); //not available metadata
-            //targetBand.setScalingFactor(1.0d);
-            //targetBand.setScalingOffset(0.0d);
-            targetBand.setSampleCoding(srcBand.getSampleCoding());
-            targetBand.setImageInfo(srcBand.getImageInfo());
-            //targetBand.setDescription(""); //TODO
-            targetBand.setSourceImage(srcBand.getSourceImage());
+                } else {
+                    band_name = "LAC_"+file_name.split("_")[0];
+                }
+                Band targetBand = new Band(band_name, srcBand.getDataType(), srcBand.getRasterWidth(), srcBand.getRasterHeight());
+                product.addBand(targetBand);
+                ProductUtils.copyGeoCoding(srcBand, targetBand);
+                targetBand.setNoDataValue(srcBand.getNoDataValue());
+                targetBand.setNoDataValueUsed(true);
+                if (visual_band) {
+                    targetBand.setSpectralWavelength((float)bandInfo.getWavelengthCentral());
+                    targetBand.setSpectralBandwidth((float)(bandInfo.getWavelengthMax() - bandInfo.getWavelengthMin()));
+                }
+                //targetBand.setScalingFactor(1.0d);
+                //targetBand.setScalingOffset(0.0d);
+                targetBand.setSampleCoding(srcBand.getSampleCoding());
+                targetBand.setImageInfo(srcBand.getImageInfo());
+                //targetBand.setDescription(""); //TODO
+                targetBand.setSourceImage(srcBand.getSourceImage());
+            }
+
+            return product;
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
         }
-
-        return product;
+        return null;
     }
 
     @Override
