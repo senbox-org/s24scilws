c46420267b05c2147c9e16c577d61027304c890b
Florian1209 <douziechflorian@gmail.com>
2021-05-12 16:10:02 +0200

    update gdal version 2.4.0

--- toposhadow/src/main/resources/auxdata/s24scilws/toposhadow/init.sh
+++ toposhadow/src/main/resources/auxdata/s24scilws/toposhadow/init.sh
@@ -1,17 +1,17 @@
 
 # Path to OrthotoolPython
-export SEOM_TOPO_DIR=$PWD
+export PYTHONPATH=/usr/bin/python3.6
+export SEOM_TOPO_DIR=${HOME}/dev/s24scilws/toposhadow/src/main/resources/auxdata/s24scilws/toposhadow
 
 # Data
 export SEOM_TOPO_DATA=${SEOM_TOPO_DIR}/seom_data/
 
-
 # Add orthotool python script to PYTHONPATH
-export SEOM_TOPO_PYTHON="${ORTHOTOOL_DIR}/seom_python/src/"
+export SEOM_TOPO_PYTHON="${SEOM_TOPO_DIR}/seom_python/src/"
 export PYTHONPATH="${SEOM_TOPO_DIR}:$PYTHONPATH"
 
 export SEOM_JAVA="${SEOM_TOPO_DIR}/seom_java/target/seom-1.0-jar-with-dependencies.jar"
+export GDAL_DIR=$HOME/custom_gdal/COTS
 
 # GDAL binding's java library path
-export GDAL_LIBRARY_PATH="${GDAL_DIR}/COTS/gdal-2.4.0/swig/java"
-
+export GDAL_LIBRARY_PATH=$GDAL_DIR/gdal-2.4.0/swig/java
